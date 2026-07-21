a43e53c146da994542d86e10370c1c4ef74b2d3d
wiggly-sheets <zmikula@icloud.com>
2026-07-20 23:20:19 -0400

    fix: update SHA256 hashes for v1.0.2 rebuild (unified app name)

--- Casks/spacemap.rb
+++ Casks/spacemap.rb
@@ -2,8 +2,8 @@ cask "spacemap" do
   arch = on_arch_conditional arm: "arm64", intel: "x86_64"
 
   version "1.0.2"
-  sha256 arm:   "ddc98d0b98c9813ebc75e79077ddccb5da406a17aebd70f0ae332e7a9ddc3ade",
-         intel: "a774698371bea9f23565a623b44264780169c53c0b721a5f0edf608b47c8e30d"
+  sha256 arm:   "78c3644217b7fa61b92bea9a6b39cbace691ddf42319667da043ff9f9e00e57d",
+         intel: "4e35ffad7acad3d77575f66cdb9132173b226f503b88cee30ba1aaaac045ac3d"
 
   url "https://github.com/wiggly-sheets/spacemap/releases/download/v#{version}/spacemap-#{version}-#{arch}.dmg"
   name "spacemap"
