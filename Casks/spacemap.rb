c808e409884af63e218c956eadaa65c7331664e2
wiggly-sheets <zmikula@icloud.com>
2026-07-20 23:08:35 -0400

    spacemap 1.0.2

---
+++ Casks/spacemap.rb
@@ -0,0 +1,21 @@
+cask "spacemap" do
+  arch = on_arch_conditional arm: "arm64", intel: "x86_64"
+
+  version "1.0.2"
+  sha256 arm:   "ddc98d0b98c9813ebc75e79077ddccb5da406a17aebd70f0ae332e7a9ddc3ade",
+         intel: "a774698371bea9f23565a623b44264780169c53c0b721a5f0edf608b47c8e30d"
+
+  url "https://github.com/wiggly-sheets/spacemap/releases/download/v#{version}/spacemap-#{version}-#{arch}.dmg"
+  name "spacemap"
+  desc "Yabai workspace visualizer"
+  homepage "https://github.com/wiggly-sheets/spacemap"
+
+  depends_on macos: ">= :ventura"
+
+  app "spacemap.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.spacemap.plist",
+    "~/.config/spacemap",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,33 @@
+# Homebrew tap for spacemap
+
+Install spacemap via Homebrew:
+
+```bash
+brew tap wiggly-sheets/spacemap https://github.com/wiggly-sheets/homebrew-spacemap.git
+brew install --cask spacemap
+```
+
+Or in one step:
+
+```bash
+brew install wiggly-sheets/spacemap/spacemap
+```
+
+## What is spacemap?
+
+A macOS utility that visualizes yabai workspaces in a floating 2D grid overlay.
+
+## Updating
+
+When a new version is released, update the tap:
+
+```bash
+brew upgrade --cask spacemap
+```
+
+## Uninstalling
+
+```bash
+brew uninstall --cask spacemap
+brew untap wiggly-sheets/spacemap
+```
